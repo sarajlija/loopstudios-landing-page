b9e04a6860636626577d0ef78497ccb4c934ec8d
Salko <64722632+sarajlija@users.noreply.github.com>
2021-03-12 09:54:12 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+//Open and close menu
+const menu = document.getElementById('menu-open'),
+    nav_mobile = document.getElementById('nav-mobile'),
+     menu_close = document.getElementById('menu-close');
+
+menu.addEventListener('click', () => {
+    nav_mobile.style.display = 'flex';
+   
+});
+
+menu_close.addEventListener('click', () => {
+    nav_mobile.style.display = 'none';
+
+  
+});
